4d6dbcf587676d6aa74eab1e859eda59344c0da1
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-10 16:18:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,10 @@
 class CashRegister
   
-def cash_register_with_discount(discount)
-  @discount = discount
-  
-end 
+
   
 def initialize(total = 0)
   @total = total
-  self.discount 
+  
 end 
 
 end 
