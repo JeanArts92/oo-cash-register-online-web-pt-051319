a7cf9e3a2b1af3017a81a033f7b456b5e130b018
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-10 16:58:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,8 +4,10 @@ attr_accessor :discount, :items, :last_transactions, :total
 @@cash_register_total = 100 
   
 def initialize(total = 0)
+  @items = []
+  @discount = discount
   @total = total
-  @discount = 20
+  
 end 
 
 
