38e9a7c80b30f68618577e0574b139a3ea4c18d5
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-10 16:34:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,13 @@
 class CashRegister
 attr_reader :discount 
 
-def discount=(discount)
+def discount=(discount = 20)
   @discount = discount
 end
   
 def initialize(total = 0)
   @total = total
-  self.discount = 20
+  self.discount / total * 100
 end 
 
 end 
